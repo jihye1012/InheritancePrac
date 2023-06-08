a218fe95b2eb9753d2306b76ac0e0c3932fdb771
Jihye Yang <s2127@e-mirim.hs.kr>
2023-06-08 15:00:12 +0900

    고양이,강아지 상속관계 실습

---
+++ InheritancePrac/Dog.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Dynamic;
+
+namespace InheritancePrac
+{
+    internal class Dog : Animal
+    {
+        public string Color { get; set; }
+
+        public void Bark()
+        {
+            Console.WriteLine("왈왈 짓습니다.");
+        }
+    }
+}
--- InheritancePrac/Program.cs
+++ InheritancePrac/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Common;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,20 @@ namespace InheritancePrac
     {
         static void Main(string[] args)
         {
+            List<Animal> animals = new List<Animal>() {
+                new Dog(), new Dog(), new Dog(),
+                new Cat(), new Cat(), new Cat()
+            };
+
+            foreach (var item in animals)
+            {
+                item.Eat();
+                item.Sleep();
+
+                ((Dog)item).Bark();
+                ((Cat)item).Meow();
+            }
         }
     }
 }
+
